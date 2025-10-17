52b4b575b7d4af616b608df03be17911895cb06e
GAGHL <ns.gaghl@gmail.com>
2025-10-17 11:09:42 +0330

    Blinky Project

---
+++ Blinky/Blinky/main.asm
@@ -0,0 +1,35 @@
+;
+; Blinky.asm
+;
+; Created: 10/17/2025 10:36:56 AM
+; Author : GAGHL
+;
+.include "tn13adef.inc"
+
+.org 0x00
+rjmp start
+
+; Replace with your application code
+start:
+    ldi r16, (1 << PB1)
+    out DDRB, r16
+
+MAIN_LOOP:
+    sbi PORTB, PB1
+    rcall DELAY
+
+    cbi PORTB, PB1
+    rcall DELAY
+
+    rjmp MAIN_LOOP
+
+DELAY:
+    ldi r18, 200
+OUTER_LOOP:
+    ldi r19, 200
+INNER_LOOP:
+    dec r19
+    brne INNER_LOOP
+    dec r18
+    brne OUTER_LOOP
+    ret
